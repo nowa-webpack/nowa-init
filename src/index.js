e2217f2f92f5ff50cbb9a6af6ffaf69d47f93cce
gbk <ck0123456@gmail.com>
2016-06-12 13:17:27 +0800

    ci

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 * @Author: gbk <ck0123456@gmail.com>
 * @Date:   2016-04-21 17:34:00
 * @Last Modified by:   gbk
-* @Last Modified time: 2016-06-12 13:08:40
+* @Last Modified time: 2016-06-12 13:17:09
 */
 
 'use strict';
@@ -10,6 +10,13 @@
 var proj = require('./proj');
 var mod = require('./mod');
 var pkg = require('../package.json');
+
+var ALIAS = {
+  h5: 'https://github.com/nowa-webpack/template-salt/archive/master.zip',
+  salt: 'https://github.com/nowa-webpack/template-salt/archive/master.zip',
+  web: 'https://github.com/nowa-webpack/template-uxcore/archive/master.zip',
+  uxcore: 'https://github.com/nowa-webpack/template-uxcore/archive/master.zip'
+};
 
 // plugin defination
 module.exports = {
@@ -32,22 +39,14 @@ module.exports = {
     if (typeof template !== 'string') {
       template = 'uxcore';
     }
+    template = ALIAS[template] || template;
 
-    // generate module
-    if (typeof a0 === 'string' && !/^https?:\/\//.test(a0)) {
-      return mod(a0, force);
-    }
-
-    // generate project
-    switch (template) {
-      case 'h5':
-      case 'salt':
-        return proj('https://github.com/nowa-webpack/template-salt/archive/master.zip', force);
-      case 'web':
-      case 'uxcore':
-        return proj('https://github.com/nowa-webpack/template-uxcore/archive/master.zip', force);
-      default:
-        return proj(template, force);
+    if (typeof a0 === 'string' && !ALIAS[a0] && !/^https?:\/\//.test(a0)) {
+      // generate module
+      mod(a0, force);
+    } else {
+      // generate project
+      proj(template, force);
     }
   }
 };
